7da8a9f781a8613b43f4c3f96f47f59c53b81b41
Broadcloak <58501742+Broadcloak@users.noreply.github.com>
2024-04-05 16:52:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,21 @@
 # main.py
 import sys
+import numpy as np
+import open3d as o3d
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QAction, QFileDialog
-from PyQt5.QtGui import QIcon
 from PyQt5.QtOpenGL import QGLWidget
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from OpenGL.GLUT import *
-from PyQt5.QtCore import Qt, QPoint
-import pywavefront
 
-vertices = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-)
-
-surfaces = (
-    (0, 1, 2, 3),
-    (3, 2, 7, 6),
-    (6, 7, 5, 4),
-    (4, 5, 1, 0),
-    (1, 5, 7, 2),
-    (4, 0, 3, 6)
-)
-
-class ModelVisualization(QGLWidget):
+class OpenGLWidget(QGLWidget):
     def __init__(self, parent=None):
-        super(ModelVisualization, self).__init__(parent)
+        super(OpenGLWidget, self).__init__(parent)
 
-        self.rotate_x = 0
-        self.rotate_y = 0
-        self.last_pos = QPoint()
+        self.mesh = None
 
     def initializeGL(self):
         glClearColor(0.0, 0.0, 0.0, 1.0)
         glEnable(GL_DEPTH_TEST)
-
-    def paintGL(self):
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glLoadIdentity()
-
-        glTranslatef(0.0, 0.0, -5.0)  # 调整模型位置
-        glRotatef(self.rotate_x, 1, 0, 0)  # 绕x轴旋转
-        glRotatef(self.rotate_y, 0, 1, 0)  # 绕y轴旋转
-
-        glBegin(GL_QUADS)
-        for surface in surfaces:
-            for vertex_i in surface:
-                glVertex3fv(vertices[vertex_i])
-        glEnd()
 
     def resizeGL(self, width, height):
         glViewport(0, 0, width, height)
@@ -62,30 +24,16 @@ class ModelVisualization(QGLWidget):
         gluPerspective(45, width / height, 0.1, 100.0)
         glMatrixMode(GL_MODELVIEW)
 
-    def mousePressEvent(self, event):
-        self.last_pos = event.pos()
-
-    def mouseMoveEvent(self, event):
-        dx = event.x() - self.last_pos.x()
-        dy = event.y() - self.last_pos.y()
-
-        self.rotate_x += dy
-        self.rotate_y += dx
-
-        self.last_pos = event.pos()
-        self.update()
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("三维模型可视化窗口")
-        self.setGeometry(900, 400, 800, 600)
+        self.setWindowTitle("三维模型可视化")
+        self.setGeometry(100, 100, 800, 600)
 
-        cube_visualization = ModelVisualization(self)
-
+        self.openGLWidget = OpenGLWidget(self)
         layout = QVBoxLayout()
-        layout.addWidget(cube_visualization)
+        layout.addWidget(self.openGLWidget)
 
         widget = QWidget()
         widget.setLayout(layout)
@@ -98,16 +46,18 @@ class MainWindow(QMainWindow):
         file_menu.addAction(import_model_action)
 
     def import_model(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "选择模型文件", "", "Model Files (*.obj *.h5 *.pb)")
+        file_path, _ = QFileDialog.getOpenFileName(self, "选择模型文件", "", "PLY Files (*.ply)")
         if file_path:
-            print("选择模型文件:", file_path)
-            model = pywavefront.Wavefront(file_path)
-            if model:
-                if self.model_visualization:
-                    self.layout().removeWidget(self.model_visualization)
-                    self.model_visualization.deleteLater()
-                self.model_visualization = ModelVisualization(model, self)
-                self.layout().addWidget(self.model_visualization)
+            print("Selected model file:", file_path)
+            try:
+                # 通过open3d直接读取网格
+                self.openGLWidget.mesh = o3d.io.read_triangle_mesh(file_path)
+                self.openGLWidget.update()
+                # 绘制网格顶点法线
+                self.openGLWidget.mesh.compute_vertex_normals()
+                o3d.visualization.draw_geometries([self.openGLWidget.mesh])
+            except Exception as e:
+                print("Error loading model:", str(e))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
